38d24d481edee3ccc0bd4b8e72bea15e91db1253
TF Nelson <tails.fanatic@gmail.com>
2013-05-30 12:54:50 -0500

    Added sidebar to manage.php

--- manage.php
+++ manage.php
@@ -1,8 +1,16 @@
 <?php require 'include/common.php'; ?>
 <html>
 	<head>
-		<title>Sparkle Bookie :: DB Management</title>
+		<title>Sparkle Bookie :: Management</title>
 	</head>
 	<body>
+<?php include 'include/template/navbar.html'; ?>
+		<hr />
+		<div class="sidebar">
+			<ul>
+				<li><a href="manage.php?tab=category">Category</a></li>
+				<li><a href="manage.php?tab=user">Users</a></li>
+			</ul>
+		</div>
 	</body>
 </html>
