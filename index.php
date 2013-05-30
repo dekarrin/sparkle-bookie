f43288d6904397ce95f8c207a69a2104770e884d
TF Nelson <tails.fanatic@gmail.com>
2013-05-30 11:52:21 -0500

    Added some server root files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require 'include/common.php'; ?>
+<html>
+	<head>
+		<title>Sparkle Bookie :: Home</title>
+	</head>
+	<body>
+		<h1>Sparkle Bookie</h1>
+		<p>This bad boy keeps track of shared finances and the like between several people.
+		It's pretty hot.</p>
+		<hr />
+<?php include 'include/template/navbar.html'; ?>
+	</body>
+</html>
---
+++ manage.php
@@ -0,0 +1,8 @@
+<?php require 'include/common.php'; ?>
+<html>
+	<head>
+		<title>Sparkle Bookie :: DB Management</title>
+	</head>
+	<body>
+	</body>
+</html>
